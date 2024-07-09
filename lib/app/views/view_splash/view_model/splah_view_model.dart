e042902005165bddd6d6a9a200d43938c07e347f
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-09 23:54:15 +0300

    add ✨: auto route

--- lib/app/views/view_splash/view_model/splah_view_model.dart
+++ lib/app/views/view_splash/view_model/splah_view_model.dart
@@ -1,21 +1,21 @@
 import 'dart:async';
+
+import 'package:auto_route/auto_route.dart';
+import 'package:bazar_books_mobile_app/app/router/app_router.dart';
 import 'package:bazar_books_mobile_app/app/views/view_splash/view_model/splash_event.dart';
 import 'package:bazar_books_mobile_app/app/views/view_splash/view_model/splash_state.dart';
 import 'package:bloc/bloc.dart';
-// import 'package:flutter/material.dart';
 
 class SplashViewModel extends Bloc<SplashEvent, SplashState> {
   SplashViewModel() : super(SplashInitialState()) {
     on<SplashInitialEvent>(_onSplashInitial);
   }
-  // AuthService _authService = AuthService();
+
   Future<FutureOr<void>> _onSplashInitial(
       SplashInitialEvent event, Emitter<SplashState> emit) async {
     Future.delayed(const Duration(seconds: 3), () async {
-      // await _authService.isSignedIn()
-      //     ? event.context.router.replace(BottomNavigationWidgetRoute())
-      //     : event.context.router.replace(OnboardingViewRoute());
-      // Navigator.pushReplacement(event.context, MaterialPageRoute(builder: (_) => OnboardingView()));
+      // Assuming you have a correct context here
+      event.context.router.replace(const OnboardingFirstViewRoute());
     });
   }
 }
