fc8721ad6754eab74ee6f20925bddac94428e022
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-10 01:10:10 +0300

    update 🆕: color

--- lib/core/constants/color_constants.dart
+++ lib/core/constants/color_constants.dart
@@ -5,4 +5,9 @@ final class ColorConstants {
   const ColorConstants._();
 
   static const Color primaryColor = Color(0xFF54408C);
+  static const Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
+
+  //text and icon color
+  static const Color textLight = Colors.black;
+  static const Color textDark = Colors.white;
 }
