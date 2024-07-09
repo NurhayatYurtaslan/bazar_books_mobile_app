d448e8964f9db7ab6e9c4a8828ac355eba456fc0
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-10 01:10:53 +0300

    add ✨: onboarding first page

--- lib/app/views/view_onboarding/onboarding_first_view.dart
+++ lib/app/views/view_onboarding/onboarding_first_view.dart
@@ -1,5 +1,11 @@
-
 import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:bazar_books_mobile_app/app/router/app_router.dart';
+import 'package:bazar_books_mobile_app/core/constants/color_constants.dart';
+import 'package:bazar_books_mobile_app/core/extensions/context_extension.dart';
+import 'package:bazar_books_mobile_app/core/widgets/custom_button_widget.dart';
+import 'package:bazar_books_mobile_app/core/widgets/text_button_widget.dart';
+import 'package:bazar_books_mobile_app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -8,6 +14,60 @@ class OnboardingFirstView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+      body: Container(
+        color: ColorConstants.backgroundColor,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(Assets.images.onboardingOne.path),
+            SizedBox(
+              height: context.sizedHeightBoxLow.height,
+            ),
+            const Text(
+              'Now reading books \nwill be easier',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: context.sizedHeightBoxLow.height,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: context.mediumValue * 2,
+                  right: context.mediumValue * 2),
+              child: Text(
+                ' Discover new worlds, join a vibrant reading community. Start your reading adventure effortlessly with us.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey.shade500,
+                    fontSize: 15),
+              ),
+            ),
+            SizedBox(height: context.sizedHeightBoxExtraHigh.height),
+            CustomButton(
+              buttonText: 'Continue',
+              onPressed: () {
+                context.router.replaceAll([const SplashViewRoute()]);
+              },
+              buttonBgColor: Theme.of(context).primaryColor,
+              buttonTextColor: ColorConstants.textDark,
+            ),
+            SizedBox(height: context.sizedHeightBoxHigh.height),
+            TextButtonWidget(
+              onPressed: () {
+                context.router.replaceAll([const SplashViewRoute()]);
+              },
+              text: 'Sign in',
+              textColor: ColorConstants.primaryColor,
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
