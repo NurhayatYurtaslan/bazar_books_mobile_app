b815d0c572e3363b6988e2924a9ec973ee75a331
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-13 17:05:30 +0300

    update 🆕: clean code onboarding state

--- lib/app/views/view_onboarding/view_model/onboarding_state.dart
+++ lib/app/views/view_onboarding/view_model/onboarding_state.dart
@@ -1,5 +1,12 @@
-class OnboardingState{
-  int currentIndex;
+abstract class OnboardingState {
+  int page;
 
-  OnboardingState(this.currentIndex);
+  OnboardingState({required this.page});
+}
+
+class OnboardingInitialState extends OnboardingState {
+  @override
+  // ignore: overridden_fields
+  int page;
+  OnboardingInitialState(this.page) : super(page: page);
 }
