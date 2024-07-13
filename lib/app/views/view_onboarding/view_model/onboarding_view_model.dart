b808a37b6cc162af9604dbc8794a15b9a516ab19
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-07-13 17:20:56 +0300

    update 🆕: clean code onboarding view model

--- lib/app/views/view_onboarding/view_model/onboarding_view_model.dart
+++ lib/app/views/view_onboarding/view_model/onboarding_view_model.dart
@@ -1,18 +1,26 @@
-
-
 import 'dart:async';
 
+import 'package:auto_route/auto_route.dart';
+import 'package:bazar_books_mobile_app/app/router/app_router.dart';
 import 'package:bazar_books_mobile_app/app/views/view_onboarding/view_model/onboarding_event.dart';
 import 'package:bazar_books_mobile_app/app/views/view_onboarding/view_model/onboarding_state.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 
 class OnboardingViewModel extends Bloc<OnboardingEvent, OnboardingState> {
-  OnboardingViewModel() : super(OnboardingState(0)) {
-    on<CurrentPageIndexEvent>(_currentPageIndex);
+  OnboardingViewModel() : super(OnboardingInitialState(0)) {
+    on<IndicatorIncrementEvent>(_onIndicatorIncrement);
   }
 
-  FutureOr<void> _currentPageIndex(
-      CurrentPageIndexEvent event, Emitter<OnboardingState> emit) {
-    emit(OnboardingState(event.currentIndex));
+  FutureOr<void> _onIndicatorIncrement(
+      IndicatorIncrementEvent event, Emitter<OnboardingState> emit) {
+    event.page++;
+    if (event.page < 3) {
+      event.pageController.animateToPage(event.page,
+          duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
+    } else {
+      event.context.router.replace(const SplashViewRoute());
+    }
+    emit(OnboardingInitialState(event.page));
   }
 }
