aa04771f6df43fa1cd701e7b55101fd9c9db3cd3
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-08-21 00:45:45 +0300

    add ✨: text widget

---
+++ lib/core/widgets/custom_text_widget.dart
@@ -0,0 +1,72 @@
+import 'package:bazar_books_mobile_app/core/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextInput extends StatelessWidget {
+  final String label;
+  final String? hintText;
+  final Icon icon;
+  final TextEditingController controller;
+  final TextInputType? keyboardType;
+  final TextInputAction? textInputAction;
+  final List<TextInputFormatter>? inputFormatters;
+  final String? Function(String?)? validator;
+
+  CustomTextInput({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.controller,
+    this.keyboardType,
+    this.textInputAction,
+    this.inputFormatters,
+    this.validator,
+    this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: context.verticalPaddingConstLow,
+          child: TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: controller,
+            keyboardType: keyboardType ?? TextInputType.text,
+            inputFormatters: inputFormatters,
+            textInputAction: textInputAction ?? TextInputAction.next,
+            validator: validator,
+            cursorColor: Colors.black,
+            decoration: InputDecoration(
+                labelStyle: TextStyle(color: Colors.black),
+                enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(context.lowRadius),
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.onBackground)),
+                focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(context.lowRadius),
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.onBackground)),
+                errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(context.lowRadius),
+                    borderSide:
+                        BorderSide(color: Theme.of(context).colorScheme.error)),
+                disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(context.lowRadius),
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.onBackground)),
+                focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(context.lowRadius),
+                    borderSide:
+                        BorderSide(color: Theme.of(context).colorScheme.error)),
+                prefixIcon: icon,
+                label: Text(label),
+                hintText: hintText),
+          ),
+        )
+      ],
+    );
+  }
+}
