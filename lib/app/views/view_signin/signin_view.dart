fa741dc5b88c9578da910088e66263e29dac2b43
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-08-24 02:58:11 +0300

    update 🆕 : widget

--- lib/app/views/view_signin/signin_view.dart
+++ lib/app/views/view_signin/signin_view.dart
@@ -7,9 +7,9 @@ import 'package:bazar_books_mobile_app/core/extensions/context_extension.dart';
 import 'package:bazar_books_mobile_app/core/widgets/custom_button_widget.dart';
 import 'package:bazar_books_mobile_app/core/widgets/custom_password_widget.dart';
 import 'package:bazar_books_mobile_app/core/widgets/custom_text_widget.dart';
+import 'package:bazar_books_mobile_app/core/widgets/signin_and_signup_text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 
 @RoutePage()
 class SignInView extends StatelessWidget {
@@ -17,79 +17,143 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create: (context) => SigninViewModel(),
-    child: BlocBuilder<SigninViewModel, SigninState>(builder: (context,state){
-      return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: context.paddingNormal,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Welcome Back 👋',
-                  style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 40, fontWeight: FontWeight.w500)),
-              Row(
-                      children: [
-                        const Text('L10n.of(context)!.dontHaveAccount',
-                            style: TextStyle(fontSize: 16)),
-                        TextButton(
-                            onPressed: () =>context.router.push(OnboardingViewRoute()),
-                            child: Text(
-                              'Signup',
-                              style:  TextStyle(
-                                      color:Theme.of(context).primaryColor, fontSize: 16),
-                            ))
-                      ],
-                    ),
-              SizedBox(
-                height: context.height * .6,
-                child: Column(
-                
-                  children: [
-                    CustomTextInput(
-                      label: 'Email',
-                      icon: const Icon(Icons.email_outlined),
-                      controller: context.read<SigninViewModel>().emailController,
-                       keyboardType: TextInputType.emailAddress,
-                       validator: (value) {
-                          if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value!)) { 
-                             return 'validEmailMessage';
+    return BlocProvider(
+      create: (context) => SigninViewModel(),
+      child:
+          BlocBuilder<SigninViewModel, SigninState>(builder: (context, state) {
+        return Scaffold(
+            body: SafeArea(
+          child: Padding(
+            padding: context.paddingNormal,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: context.mediumValue * 3,
+                  ),
+                  const SigninAndSignupTextWidget(
+                    headerText: 'Welcome Back 👋',
+                    text: 'Sign to your account',
+                  ),
+                  SizedBox(
+                    height: context.lowValue,
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomTextInput(
+                        label: 'Your email',
+                        controller:
+                            context.read<SigninViewModel>().emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) {
+                          if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value!)) {
+                            return 'validEmailMessage';
                           }
-                          
                           return null;
                         },
-                    ),
-                     CustomPasswordTextField(
-                      controller: context.read<SigninViewModel>().passwordController,
-                       validator: (value) {
-                          if (!RegExp(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$").hasMatch(value!)) {
+                      ),
+                      CustomPasswordTextField(
+                        controller:
+                            context.read<SigninViewModel>().passwordController,
+                        validator: (value) {
+                          if (!RegExp(
+                                  r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
+                              .hasMatch(value!)) {
                             return 'Error';
                           }
                           return null;
                         },
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    CustomButton(
-                      onPressed: () {
-                        context
-                            .read<SigninViewModel>()
-                            .add(SigninInitialEvent(context));
-                      },
-                      buttonText: 'Login', buttonBgColor: Theme.of(context).primaryColor, buttonTextColor: Colors.black,
-                    ),
-                    
-                  ],
-                ),
-              )
-            ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: InkWell(
+                          onTap: () =>
+                              context.router.push(OnboardingViewRoute()),
+                          child: Text(
+                            "Forgot Password?",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Theme.of(context).primaryColor),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CustomButton(
+                        onPressed: () {
+                          context
+                              .read<SigninViewModel>()
+                              .add(SigninInitialEvent(context));
+                        },
+                        buttonText: 'Login',
+                        buttonBgColor: Theme.of(context).primaryColor,
+                        buttonTextColor: Colors.white,
+                      ),
+                      SizedBox(
+                        height: context.mediumValue,
+                      ),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "Don't have an account? ",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          InkWell(
+                              onTap: () =>
+                                  context.router.push(OnboardingViewRoute()),
+                              child: Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20),
+                              )),
+                        ],
+                      ),
+                      SizedBox(
+                        height: context.mediumValue,
+                      ),
+                      const Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey,
+                              thickness: 1,
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              'Or with',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey,
+                              thickness: 1,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
-    ));
-    }),
+        ));
+      }),
     );
   }
 }
