bc8e4e6dd960e237708937e12f356609b24cb35a
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-08-25 18:55:57 +0300

    fix 💥: android and ios error

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:bazar_books_mobile_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
